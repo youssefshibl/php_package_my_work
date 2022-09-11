cfe8991e7e18246acc0c07a77a06f4ac036bfb23
youssefshibl <youssefshibl000@gmail.com>
2022-09-11 05:13:54 +0200

    first commit

---
+++ src/HandelRes.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Test;
+
+class HandelRes
+{
+    public function test()
+    {
+        return 'nour';
+    }
+
+    public function Rand($length = 10)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}
