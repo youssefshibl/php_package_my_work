22b6fe370be10affaa4983459043dc021c61fa14
youssefshibl <youssefshibl000@gmail.com>
2022-09-27 09:27:11 +0200

    two

--- src/Nexmo/Nexmo.php
+++ src/Nexmo/Nexmo.php
@@ -2,9 +2,17 @@
 
 namespace Shebl\Nexmo;
 
+use GuzzleHttp\Client;
 use Shebl\Helper\Curl;
 
 class Nexmo
 {
     use Curl;
+    public function test()
+    {
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+        var_dump($response);
+    }
 }
